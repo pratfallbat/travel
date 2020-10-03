d14733c39ce9db74af635effe6e48b140cc3bc28
pratyush kumar <pratyush845@gmail.com>
2020-10-03 13:49:52 +0530

    bar using three div

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,22 +1,2 @@
-// var $ = require("jquery");
-// var dummy = require("./dummy");
-
-// var person = require("./person");
-// now that o ur project supports es6
-import Person from "./person";
-
-class Adult extends Person {
-  payTaxes() {
-    console.log(this.name + "now have paid taxes");
-  }
-}
-
-var john = new Person("John doe ", "blue");
-john.greet();
-var jane = new Adult("Jane smith ", "orange");
-jane.greet();
-jane.payTaxes();
-
-// dummy.funcexample();
-// console.log(dummy.examplefunc);
-// $("h1").remove();
+import MobileMenu from "./modules/mobilemenu";
+var mobileMenu = new MobileMenu();
---
+++ app/assets/scripts/modules/mobilemenu.js
@@ -0,0 +1,30 @@
+import $ from "jquery";
+class MobileMenu {
+  constructor() {
+    //   this is spaghetti
+    //   its selecting element from DOM
+    //   event handlingh
+    //   defining functionality
+    // $(".site-header__menu-icon").click(function () {
+    //   console.log("the top right icon is clicked");
+    // });
+
+    this.menuIcon = $(".site-header__menu-icon");
+    this.menuContent = $(".site-header__menu-content");
+
+    this.events();
+  }
+  events() {
+    this.menuIcon.click(this.toggleTheMenu.bind(this));
+    //   in  the context of click this is changed for toggleMenu
+    console.log("_+______________________");
+    console.log(this);
+  }
+  toggleTheMenu() {
+    // this.remove(); will remove it from the page
+    console.log("_+______________________");
+    console.log(this);
+    this.menuContent.toggleClass("site-header__menu-content--is-visible");
+  }
+}
+export default MobileMenu;
