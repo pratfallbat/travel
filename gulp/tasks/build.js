ad0b833e814dcfa37efd4187eeb321199e84b127
pratyush kumar <pratyush845@gmail.com>
2020-10-04 00:02:50 +0530

    change dist to docs github

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -11,16 +11,16 @@ gulp.task("previewDist", function () {
   browserSync.init({
     notify: false,
     server: {
-      baseDir: "dist",
+      baseDir: "docs",
     },
   });
 });
 
-gulp.task("deleteDistFolder", function () {
-  return del("./dist");
+gulp.task("deleteDistFolder", ["icons"], function () {
+  return del("./docs");
 });
 
-gulp.task("optimizeImages", ["deleteDistFolder", "icons"], function () {
+gulp.task("optimizeImages", ["deleteDistFolder"], function () {
   return gulp
     .src([
       "./app/assets/images/**/*",
@@ -34,9 +34,14 @@ gulp.task("optimizeImages", ["deleteDistFolder", "icons"], function () {
         mulipass: true,
       })
     )
-    .pipe(gulp.dest("./dist/assets/images"));
+    .pipe(gulp.dest("./docs/assets/images"));
 });
-gulp.task("usemin", ["deleteDistFolder", "styles", "scripts"], function () {
+
+gulp.task("useminTrigger", ["deleteDistFolder"], function () {
+  gulp.start("usemin");
+});
+
+gulp.task("usemin", ["styles", "scripts"], function () {
   return gulp
     .src("./app/index.html")
     .pipe(
@@ -59,7 +64,7 @@ gulp.task("usemin", ["deleteDistFolder", "styles", "scripts"], function () {
         ],
       })
     )
-    .pipe(gulp.dest("./dist"));
+    .pipe(gulp.dest("./docs"));
 });
 
-gulp.task("build", ["deleteDistFolder", "optimizeImages", "usemin"]);
+gulp.task("build", ["deleteDistFolder", "optimizeImages", "useminTrigger"]);
